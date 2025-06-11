4ef1ea2dd671019391a575fc5acd2f5224e1b4d0
Prince <juniortlou212@gmail.com>
2025-06-11 15:37:29 +0200

    Added more features and functions

--- ITCOA.ps1
+++ ITCOA.ps1
@@ -59,10 +59,16 @@ function Display_Menu2 () {
     Write-Host "2. Create Organisational units and user account"
     Write-Host "3. Exit"
 }
-function OUStructure (){
+function OUStructure () {
     $domain = "DC=Traction,DC=local"
+    $employeeOU = $null
+    try {
+        $employeeOU = Get-ADOrganizationalUnit -Filter 'Name -eq "Employee"' -SearchBase $domain -ErrorAction Stop
+    } catch {
+        # Do nothing
+    }
 
-    if (-not (Get-ADOrganizationalUnit -Filter 'Name -eq "Employee"' -SearchBase $domain -ErrorAction SilentlyContinue)) {
+    if (-not $employeeOU) {
         New-ADOrganizationalUnit -Name "Employee" -Path $domain
         Write-Host "Created OU=Employee"
     }
@@ -71,22 +77,43 @@ function OUStructure (){
 
     foreach ($dept in $departments) {
         $ouPath = "OU=Employee,$domain"
-        if (-not (Get-ADOrganizationalUnit -Filter "Name -eq '$dept'" -SearchBase $ouPath -ErrorAction SilentlyContinue)) {
+        $deptOU = $null
+        try {
+            $deptOU = Get-ADOrganizationalUnit -Filter "Name -eq '$dept'" -SearchBase $ouPath -ErrorAction Stop
+        } catch {
+            # Do nothing
+        }
+
+        if (-not $deptOU) {
             New-ADOrganizationalUnit -Name $dept -Path $ouPath
             Write-Host "Created OU=$dept under Employee"
         }
-        $computersOU = "OU=Computers,OU=$dept,OU=Employee,$domain"
-        if (-not (Get-ADOrganizationalUnit -Filter 'Name -eq "Computers"' -SearchBase "OU=$dept,OU=Employee,$domain" -ErrorAction SilentlyContinue)) {
+
+        $computersOU = $null
+        try {
+            $computersOU = Get-ADOrganizationalUnit -Filter 'Name -eq "Computers"' -SearchBase "OU=$dept,OU=Employee,$domain" -ErrorAction Stop
+        } catch {
+            # Do nothing
+        }
+
+        if (-not $computersOU) {
             New-ADOrganizationalUnit -Name "Computers" -Path "OU=$dept,OU=Employee,$domain"
             Write-Host "Created OU=Computers under OU=$dept"
         }
 
-              if (-not (Get-ADOrganizationalUnit -Filter 'Name -eq "Users"' -SearchBase "OU=$dept,OU=Employee,$domain" -ErrorAction SilentlyContinue)) {
+        $usersOU = $null
+        try {
+            $usersOU = Get-ADOrganizationalUnit -Filter 'Name -eq "Users"' -SearchBase "OU=$dept,OU=Employee,$domain" -ErrorAction Stop
+        } catch {
+            #Do nothing
+        }
+        if (-not $usersOU) {
             New-ADOrganizationalUnit -Name "Users" -Path "OU=$dept,OU=Employee,$domain"
             Write-Host "Created OU=Users under OU=$dept"
         }
     }
 }
+
 
 function Deliverable_2 () {
     OUStructure
@@ -105,7 +132,7 @@ function Deliverable_2 () {
                 -LogPath "C:\Windows\NTDS" `
                 -SysvolPath "C:\Windows\SYSVOL" `
                 -InstallDns:$true `
-                -SafeModeAdministratorPassword (ConvertTo-SecureString "YourSafeModePassword" -AsPlainText -Force) `
+                -SafeModeAdministratorPassword (ConvertTo-SecureString "Password1@" -AsPlainText -Force) `
                 -Force:$true
 
             Write-Host "Domain controller promotion initiated. Server will reboot automatically."
@@ -160,63 +187,115 @@ function Deliverable_2 () {
 function Display_Menu3 (){
     Write-Host "1.Configure DNS"
     Write-Host "2.Configure DCP"
+    Write-Host "3.Exit"
 }
+function Deliverable_3(){
+    $choice = Read-Host "Pick an option 1-3"
 
-function Deliverable_3 () {
-    while($true){
-        Display_Menu3
-        $choice=Read-Host "Pick an option:"
-        if($choice -eq '1'){
-            Install-WindowsFeature -Name DNS -IncludeManagementTools
-            Add-DnsServerPrimaryZone -Name "traction.local"
-                -Zonefile "traction.local.dns" `
-                -DynamicUpdate Secure 
-            Add-DnsServerPrimaryZone -NetworkId "192.168.1.0/24"
-            -ZoneFile "1.168.192.in-addr.arpa.dns" 
-            -DynamicUpdate Secure
-
-            #Add records for server and computers
-            Add-DnsServerResourceRecordA -Name "TRACTION-SRV01" 
-                -ZoneName "traction.local" 
-                -IPv4Address "192.168.1.10"
-            Add-DnsServerResourceRecordA -Name "Amanuel-PC" 
-                -ZoneName "traction.local" 
-                -IPv4Address "192.168.1.105"
-        }elseif ($choice -eq '2'){
-            Install-WindowsFeature -Name DHCP -IncludeManagementTools
-            Add-DhcpServerv4Scope -Name "TractionScope"` 
-                -StartRange 192.168.1.100 `
-                -EndRange 192.168.1.200 `
-                -SubnetMask 255.255.255.0 `
-                -State Active
-            Set-DhcpServerv4OptionValue -ScopeId 192.168.1.0 `
-                -Router 192.168.1.1 `
-                -DnsServer 192.168.1.10 `
-                -DnsDomain "traction.local"
-
-            Add-DhcpServerInDc -DnsName "TRACTION-SRV01.traction.local" 
-                -IpAddress 192.168.1.10 
-            Add-DhcpServer4Reservation ` 
-                -ScopeId 192.168.1.0 `
-                -IPAddress 192.168.1.105 `
-                 -ClientId "00-11-22-33-44-55" `
-                 -Description "Alex Manuel Workstation" 
-        }elseif ($choice -eq '3'){
-            Write-Host "Exiting Deliverable 3 Menu"
-            break
-        }else {
-            Write-Host "Invalid input "
+    if ($choice -eq '1') {
+        Install-WindowsFeature -Name DNS -IncludeManagementTools
+        if (-not (Get-DnsServerZone -Name "traction.local" -ErrorAction SilentlyContinue)) {
+            Add-DnsServerPrimaryZone `
+                -Name "traction.local" `
+                -ZoneFile "traction.local.dns" `
+                -DynamicUpdate Secure `
+                -ReplicationScope "Forest"
         }
+        if (-not (Get-DnsServerZone -Name "1.168.192.in-addr.arpa" -ErrorAction SilentlyContinue)) {
+            Add-DnsServerPrimaryZone `
+                -NetworkId "192.168.1.0/24" `
+                -ZoneFile "1.168.192.in-addr.arpa.dns" `
+                -DynamicUpdate Secure `
+                -ReplicationScope "Forest"
+        }
+        Add-DnsServerResourceRecordA `
+            -Name "TRACTION-SRV01" `
+            -ZoneName "traction.local" `
+            -IPv4Address "192.168.1.10"
+
+        Add-DnsServerResourceRecordA `
+            -Name "Amanuel-PC" `
+            -ZoneName "traction.local" `
+            -IPv4Address "192.168.1.105"
+
+    } elseif ($choice -eq '2') {
+        Install-WindowsFeature -Name DHCP -IncludeManagementTools
+        Add-DhcpServerv4Scope -Name "Main Scope" -StartRange 192.168.1.100 -EndRange 192.168.1.200 -SubnetMask 255.255.255.0
+
+        Set-DhcpServerv4OptionValue -ScopeId 192.168.1.0 -Router 192.168.1.1
+        Set-DhcpServerv4OptionValue -ScopeId 192.168.1.0 -DnsServer 192.168.1.10
+        Set-DhcpServerv4OptionValue -ScopeId 192.168.1.0 -DnsDomain "Traction.local"
+        Add-DhcpServerInDC -DnsName "TRACTION-SRV01.Traction.local" -IPAddress 192.168.1.10
+        Add-DhcpServerv4Reservation -ScopeId 192.168.1.0 -IPAddress 192.168.1.105 -ClientId "00-11-22-33-44-55" -Description "Alex Manuel PC"
+
+    } else {
+        Write-Host "Invalid choice. Please choose 1 for DNS or 2 for DHCP."
     }
 }
-
 function Display_Menu4(){
     Write-Host "1.Create GPO for Sales"
     Write-Host "2.Create GPO for Engineering Policy"
     Write-Host "3.Create a GPO for all users"
     Write-Host "4. Create a GPO Domain password policy "
+    Write-Host "5. Exit"
+}
+function Deliverable_4 {
+    while ($true) {
+        Display_Menu4
+        $choice = Read-Host "Enter your choice"
+
+        if ($choice -eq "1") {
+            $gpoName = "Sales Policy"
+            if (-not (Get-GPO -Name $gpoName -ErrorAction SilentlyContinue)) {
+                New-GPO -Name $gpoName
+                New-GPLink -Name $gpoName -Target "OU=Sales,OU=Employee,DC=Traction,DC=local"
+                Write-Host "GPO '$gpoName' created and linked to Sales OU."
+            } else {
+                Write-Host "GPO '$gpoName' already exists."
+            }
+
+        } elseif ($choice -eq "2") {
+            $gpoName = "Engineering Policy"
+            if (-not (Get-GPO -Name $gpoName -ErrorAction SilentlyContinue)) {
+                New-GPO -Name $gpoName
+                New-GPLink -Name $gpoName -Target "OU=Engineering,OU=Employee,DC=Traction,DC=local"
+                Write-Host "GPO '$gpoName' created and linked to Engineering OU."
+            } else {
+                Write-Host "GPO '$gpoName' already exists."
+            }
+
+        } elseif ($choice -eq "3") {
+            $gpoName = "All Users Policy"
+            if (-not (Get-GPO -Name $gpoName -ErrorAction SilentlyContinue)) {
+                New-GPO -Name $gpoName
+                New-GPLink -Name $gpoName -Target "OU=Employee,DC=Traction,DC=local"
+                Write-Host "GPO '$gpoName' created and linked to Employee OU."
+            } else {
+                Write-Host "GPO '$gpoName' already exists."
+            }
+
+        } elseif ($choice -eq "4") {
+            $gpoName = "Domain Password Policy"
+            if (-not (Get-GPO -Name $gpoName -ErrorAction SilentlyContinue)) {
+                New-GPO -Name $gpoName
+                Set-GPRegistryValue -Name $gpoName -Key "HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" -ValueName "MaximumPasswordAge" -Type DWord -Value 30
+                New-GPLink -Name $gpoName -Target "DC=Traction,DC=local"
+                Write-Host "GPO '$gpoName' created and linked to the domain for password policy."
+            } else {
+                Write-Host "GPO '$gpoName' already exists."
+            }
+
+        } elseif ($choice -eq "5") {
+            Write-Host "Exiting GPO menu."
+            break
+
+        } else {
+            Write-Host "Invalid option. Please choose between 1 and 5.`n"
+        }
+    }
 }
 
-function Deliverable_4 () {
-
-}
+Deliverable_1
+Deliverable_2
+Deliverable_3
+Deliverable_4
